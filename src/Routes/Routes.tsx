811a0b7946c2d9b6bcfc528bef3a767f4dac706b
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-09-23 17:27:40 -0300

    feat(routes): added and configured react-routes enviroment

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,11 @@
+import * as ReactRouter from "react-router-dom";
+
+
+const AppRoutes = ReactRouter.createBrowserRouter([
+    {
+        path: '/',
+        element: <div><p>First Route With React-Router</p></div>
+    }
+]);
+
+export default AppRoutes;  
