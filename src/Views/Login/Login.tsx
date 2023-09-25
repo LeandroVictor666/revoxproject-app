6abc7300cfff59c6c1eb9f4fe5d8ebd4793407e1
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-09-25 01:46:11 -0300

    style(login.component): stylized login component

--- src/Views/Login/Login.tsx
+++ src/Views/Login/Login.tsx
@@ -3,13 +3,38 @@ const Login = (): JSX.Element => {
   return (
     <div className={Styles.loginContainer}>
       <form className={Styles.loginForm}>
-        <div className={Styles.loginInputBox}>
+        <div className={`${Styles.usernameInputBox} ${Styles.loginInputBox}`}>
           <p>Username</p>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            placeholder="Input Your Username"
+            autoComplete="off"
+          />
         </div>
-        <div className={Styles.loginInputBox}>
+        <div className={`${Styles.passwordInputBox} ${Styles.loginInputBox}`}>
           <p>Password</p>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Input Your Secret Password!"
+          />
+        </div>
+        <input type="submit" style={{ display: "none" }} />
+
+        <div className={Styles.loginControls}>
+          <button className={Styles.loginBtn}>
+            <p>Login</p>
+          </button>
+          <a href="forgot-password" id={Styles.forgotPasswordId}>
+            Forgot Password?
+          </a>
+          <div className={Styles.bottomControls}>
+            <div className={Styles.horizontalLine}></div>
+            <button className={Styles.registerBtn}>
+              <p>Create New Account</p>
+            </button>
+          </div>
         </div>
       </form>
     </div>
