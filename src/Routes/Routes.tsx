61c1953463577e51dbcaac52032e7343420acb7e
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-09-29 19:23:58 -0300

    feat(404.view): added 404 not found view/route

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,19 +2,24 @@ import * as ReactRouter from "react-router-dom";
 import HomePageView from "../Views/Home/HomePageView";
 import LoginView from "../Views/Login/LoginView";
 import RegisterView from "../Views/Register/RegisterView";
+import NotFound from "../Views/404/NotFound";
 
 const AppRoutes = ReactRouter.createBrowserRouter([
   {
     path: "/",
-    element: <HomePageView/>,
+    element: <HomePageView />,
   },
   {
     path: "/login",
-    element: <LoginView/>,
+    element: <LoginView />,
   },
   {
     path: "/register",
-    element: <RegisterView/>,
+    element: <RegisterView />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
---
+++ src/Views/404/NotFound.tsx
@@ -0,0 +1,12 @@
+import Styles from "../../Styles/app.module.css";
+const NotFound = () => {
+  return (
+    <main className={Styles.NotFoundMain}>
+      <div className={Styles.bodyContent}>
+        <h1>404 | Not Found</h1>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
