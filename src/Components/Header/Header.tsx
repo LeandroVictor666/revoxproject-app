ee4e9d90bc47366b97f02fe75f88df2dfe1f8b3a
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-04 09:48:57 -0300

    refactor(component): componentized Header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,16 +1,20 @@
 import Styles from "../../Styles/app.module.css";
+import AccountMenu from "../AccountMenu/AccountMenu";
 import HeaderLastSection from "./HeaderLastSection";
 
 const Header = (): JSX.Element => {
   return (
-    <header className={Styles.pageHeader}>
-      <div className={Styles.containerFixer}>
-        <div className={Styles.firstSection}>
-          <p>RevoXProject</p>
+    <>
+      <header className={Styles.pageHeader}>
+        <div className={Styles.containerFixer}>
+          <div className={Styles.firstSection}>
+            <p>RevoXProject</p>
+          </div>
+          <HeaderLastSection />
         </div>
-        <HeaderLastSection />
-      </div>
-    </header>
+      </header>
+      <AccountMenu />
+    </>
   );
 };
 
