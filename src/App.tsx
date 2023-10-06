076e622b5a6492855fb34661469205b8edd0a71e
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-06 09:32:28 -0300

    feat(component): added 'AuthSection' "Component"
    this function will just call 'propagateAuthentication' method from 'AuthenticationService' in the
    start of the application

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,24 @@ import Header from "./Components/Header/Header";
 import * as ReactRedux from "react-redux";
 import store from "./Redux/Store";
 import ModalComponent from "./Components/Modal/ModalComponent";
+import AuthenticationService from "./Services/Authentication.Service";
+
+/**
+ * This function is just to auth the user.
+ * @returns nothing
+ */
+const AuthSection = () => {
+  const dispatcher = ReactRedux.useDispatch();
+  const authenticationService = new AuthenticationService();
+  authenticationService.propagateAuthentication(dispatcher);
+  return <></>;
+};
+
 function App() {
   return (
     <>
       <ReactRedux.Provider store={store}>
+        <AuthSection></AuthSection>
         <ModalComponent></ModalComponent>
         <Header></Header>
         <RouterProvider router={AppRoutes} />
