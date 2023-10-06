7223a1bddd6010f99898c220aea0d78c1aafaaa2
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-06 09:35:23 -0300

    feat(component): 'Header Last Section' now get account informations from react-redux

--- src/Components/Header/HeaderLastSection.tsx
+++ src/Components/Header/HeaderLastSection.tsx
@@ -1,18 +1,12 @@
+import { useSelector } from "react-redux";
 import AccountDto from "../../DTO/AccountDto";
 import Styles from "../../Styles/app.module.css";
 import GearIcon from "../../assets/icons/GearIcon";
 import HamburgMenu from "../../assets/icons/HamburgMenu";
+import { IReducerProps } from "../../Redux/Store";
 const HeaderLastSection = (): JSX.Element => {
-  const isAuthenticated = true;
-  const myAccount: AccountDto = {
-    id: 0,
-    username: "leandro-victor-666",
-    nickname: "Leandro Victor",
-    email: "leandrovictordev@gmail.com",
-    birthday: new Date(2004, 6, 14),
-  };
-
-  if (!isAuthenticated) {
+  const authState = useSelector((state: IReducerProps) => state.accountAuth);
+  if (!authState.isAuthenticated) {
     return (
       <div className={Styles.lastSection}>
         <nav>
@@ -26,11 +20,20 @@ const HeaderLastSection = (): JSX.Element => {
       </div>
     );
   }
+
+  const myAccount: AccountDto = {
+    id: authState.accountDataObject.id,
+    username: authState.accountDataObject.username,
+    nickname: authState.accountDataObject.nickname,
+    email: authState.accountDataObject.email,
+    birthday: authState.accountDataObject.birthday,
+  };
   return (
     <div className={Styles.lastSectionAuthenticated}>
       <a href={`profile/${myAccount.id}`}>
         <img
           src={`/Assets/UsersPfp/user-${myAccount.id}.jpeg`}
+          srcSet="/Assets/UsersPfp/default.jpeg"
         ></img>
       </a>
       <HamburgMenu />
