e895729c14d3b16ec025f38251d28a69fd720cc8
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-07 08:10:52 -0300

    feat(component): now pfp is get by 'getPfpSrc' method from AccountService

--- src/Components/Header/HeaderLastSection.tsx
+++ src/Components/Header/HeaderLastSection.tsx
@@ -4,6 +4,7 @@ import Styles from "../../Styles/app.module.css";
 import GearIcon from "../../assets/icons/GearIcon";
 import HamburgMenu from "../../assets/icons/HamburgMenu";
 import { IReducerProps } from "../../Redux/Store";
+import AccountService from "../../Services/Account.Service";
 const HeaderLastSection = (): JSX.Element => {
   const authState = useSelector((state: IReducerProps) => state.accountAuth);
   if (!authState.isAuthenticated) {
@@ -26,15 +27,14 @@ const HeaderLastSection = (): JSX.Element => {
     username: authState.accountDataObject.username,
     nickname: authState.accountDataObject.nickname,
     email: authState.accountDataObject.email,
+    isPfpSet: authState.accountDataObject.isPfpSet,
     birthday: authState.accountDataObject.birthday,
   };
+  const accountService = new AccountService(myAccount);
   return (
     <div className={Styles.lastSectionAuthenticated}>
       <a href={`profile/${myAccount.id}`}>
-        <img
-          src={`/Assets/UsersPfp/user-${myAccount.id}.jpeg`}
-          srcSet="/Assets/UsersPfp/default.jpeg"
-        ></img>
+        <img src={accountService.getPfpSrc()}></img>
       </a>
       <HamburgMenu />
       <GearIcon />
