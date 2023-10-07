2314321eee65c1277aac61822cb8c1594837520d
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-07 08:11:22 -0300

    feat(dto): now AccountDto have 'isPfpSet' property

--- src/DTO/AccountDto.ts
+++ src/DTO/AccountDto.ts
@@ -3,19 +3,21 @@ export default class AccountDto {
   username: string;
   nickname: string;
   email: string;
+  isPfpSet: boolean;
   birthday: Date;
-
   constructor(
     id: number,
     username: string,
     nickname: string,
     email: string,
+    isPfpSet: boolean,
     birthday: Date
   ) {
     this.id = id;
     this.username = username;
     this.nickname = nickname;
     this.email = email;
+    this.isPfpSet = isPfpSet;
     this.birthday = birthday;
   }
 }
