d471c971989f2923ba1e81364323cec4ad9ed672
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-11 20:47:30 -0300

    feat(component): removed fake publications object, now the component will get real publications

--- src/Views/Home/HomePageView.tsx
+++ src/Views/Home/HomePageView.tsx
@@ -1,38 +1,12 @@
 import MakePublicationComponent from "../../Components/MakePublication/MakePublication";
-import PublicationCard from "../../Components/PublicationCard/PublicationCard";
-import PublicationDto from "../../DTO/PublicationDto";
 import Styles from "../../Styles/app.module.css";
-import { faker } from "@faker-js/faker";
+import PublicationsComponent from "./Publications";
 const HomePageView = (): JSX.Element => {
-  const fakePublicationDto: PublicationDto = {
-    authorId: 0,
-    publicationId: 0,
-    authorNickname: "Leandro Victor",
-    authorUsername: "leandro-victor-666",
-    publicationCommentsCounter: 1,
-    publicationContent: faker.lorem.text(),
-    publicationDate: new Date("2023-09-24T05:32:45"),
-    publicationLikesCounter: 1,
-    publicationSharesCounter: 1,
-  };
-  const fakePublicationDto2: PublicationDto = {
-    authorId: 0,
-    publicationId: 0,
-    authorNickname: "Leandro Victor",
-    authorUsername: "leandro-victor-666",
-    publicationCommentsCounter: 1,
-    publicationContent: faker.lorem.text(),
-    publicationDate: new Date("2023-09-24T05:32:45"),
-    publicationLikesCounter: 1,
-    publicationSharesCounter: 1,
-  };
-
   return (
     <main className={Styles.homePageMain}>
       <MakePublicationComponent></MakePublicationComponent>
       <div className={Styles.publicationsContainer}>
-        <PublicationCard publicationDto={fakePublicationDto} />
-        <PublicationCard publicationDto={fakePublicationDto2} />
+        <PublicationsComponent />
       </div>
     </main>
   );
