e601ebffd1ed1fc2e5f027ae221e42b43adce294
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-12 23:30:06 -0300

    feat(methods/fetch/lazy loading): updated methods
    added a limiter (throttler) to PublicationsFetch in Publication Controller, preparing the enviroment
    to lazy loading

--- src/Controllers/Publication/PublicationController.ts
+++ src/Controllers/Publication/PublicationController.ts
@@ -3,13 +3,26 @@ import PublicationService from "../../Services/Publication.Service";
 
 export default class PublicationController {
   private publicationService: PublicationService;
+  private lastPublicationFetch: Date;
   constructor() {
     this.publicationService = new PublicationService();
+    this.lastPublicationFetch = new Date(2000, 10, 10);
   }
-  async fetchPublications(
-    lastPublicationId: number = 0
-  ): Promise<PublicationsDto> {
+  lastFetchIsMoreThan25SecondsAgo() {
+    const currentTime = new Date().getTime();
+    const lastFetchTime = this.lastPublicationFetch.getTime();
+    const twentyFiveInMilliseconds = 25 * 1000;
+    if (currentTime - lastFetchTime < twentyFiveInMilliseconds) {
+      return false;
+    }
+    return true;
+  }
+  async fetchPublications(lastPublicationId: number): Promise<PublicationsDto> {
+    if (this.lastFetchIsMoreThan25SecondsAgo() === false) {
+      return { count: 0, publications: [] };
+    }
     try {
+      this.lastPublicationFetch = new Date();
       const publications = await this.publicationService.getPublications(
         lastPublicationId
       );
--- src/Views/Home/Publications.tsx
+++ src/Views/Home/Publications.tsx
@@ -6,23 +6,56 @@ import PublicationCard from "../../Components/PublicationCard/PublicationCard";
 const PublicationsComponent = (): JSX.Element => {
   const publicationController = new PublicationController();
   const [alreadyFirstLoad, setAlreadyFirstLoad] = useState<boolean>(false);
-  const [publications, setPublication] = useState<PublicationsDto>({
+  const [publications, setPublications] = useState<PublicationsDto>({
     count: 0,
     publications: [],
   });
-  if (alreadyFirstLoad === false) {
-    publicationController
-      .fetchPublications()
-      .then((response) => {
-        setPublication(response);
-        setAlreadyFirstLoad(true);
-      })
-      .catch((error) => {
-        console.log(`error: ${error}`);
-      });
-  }
+  const [lastPublicationsId, setLastPublicationsId] = useState(0);
+  const [continueLoading, setContinueLoading] = useState<boolean>(true);
+  const handleScroll = () => {
+    const windowHeight = window.innerHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollPosition = window.scrollY;
+    const distanceFromBottom = scrollHeight - (scrollPosition + windowHeight);
+    const threshold = 120;
+    if (distanceFromBottom <= threshold && continueLoading === true) {
+      if (publicationController.lastFetchIsMoreThan25SecondsAgo() === false) {
+        return;
+      }
+      publicationController
+        .fetchPublications(lastPublicationsId)
+        .then((response) => {
+          //25 is because the LIMIT of search query in API is 25, if is different of 25, it means that we have reached maximum publications.
+          if (response.count == 0 || publications.count != 25) {
+            setContinueLoading(false);
+            return;
+          }
+          const newPublications: PublicationsDto = {
+            count: response.count,
+            publications: publications.publications,
+          };
+          newPublications.publications.push(...response.publications);
+          setPublications(newPublications);
+          setLastPublicationsId(
+            newPublications.publications[
+              newPublications.publications.length - 1
+            ].id
+          );
+        });
+    }
+  };
+  window.addEventListener("scroll", handleScroll);
 
-  if (publications.count !== 0) {
+  if (alreadyFirstLoad === false && continueLoading === true) {
+    publicationController.fetchPublications(0).then((response) => {
+      setPublications(response);
+      setAlreadyFirstLoad(true);
+      setLastPublicationsId(
+        response.publications[response.publications.length - 1].id
+      );
+    });
+  }
+  if (publications.count !== 0 && publications.publications != null) {
     return (
       <>
         {publications.publications.map((publication) => {
